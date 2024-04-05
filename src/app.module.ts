bed85489e79d356974eb474d091098db095bdad0
Xavier-Mayer <zazageek@gmail.com>
2024-04-05 10:20:51 +0200

    Fin backend normalement

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AnimalsModule } from './animals/animals.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/mongo-dbduo'), AnimalsModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/projetanimal-container'), AnimalsModule],
   controllers: [AppController],
   providers: [AppService],
 })
